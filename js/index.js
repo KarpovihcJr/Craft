f04d20490362c62830620dbfb1067ea10dbd5364
KarpovihcJr <karparoman98@gmail.com>
2021-02-21 22:22:48 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 
  const ware = Array.from(document.querySelectorAll('div.slider-ithem'));
 
-
 let currIdx = 0;
 
 function showCurrentIthem () {
@@ -9,3 +8,17 @@ function showCurrentIthem () {
    wareContainer.innerHTML = ware[currIdx].innerHTML;
 }
 showCurrentIthem ();
+function nextIthem () {
+currIdx++
+if (currIdx >= ware.length) currIdx = 0;
+showCurrentIthem()
+}
+function prevIthem () {
+    currIdx--
+    if (currIdx < 0) currIdx= ware.length - 1;
+    showCurrentIthem()
+}
+
+
+document.getElementById('show-prev').addEventListener('click', prevIthem)
+document.getElementById('show-next').addEventListener('click',nextIthem)
