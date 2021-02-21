55863106ace7b6af7fc3df80bbc9524f85f597d6
KarpovihcJr <karparoman98@gmail.com>
2021-02-21 21:55:48 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-(function(){
-    const mobile = document.querySelector('.mobile-menu')
-    mobile.addEventListener('click', () => {
-        mobile.classList.toggle('.menu')
 
-    })}
-    )
+ const ware = Array.from(document.querySelectorAll('div.slider-ithem'));
+
+
+let currIdx = 0;
+
+function showCurrentIthem () {
+   const wareContainer = document.querySelector('.slider');
+   wareContainer.innerHTML = ware[currIdx].innerHTML;
+}
+showCurrentIthem ();
