c4206c05470cf96b865f4c25ab42fe8c2deb778a
KarpovihcJr <karparoman98@gmail.com>
2021-02-24 15:20:48 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,24 +1,22 @@
 
- const ware = Array.from(document.querySelectorAll('div.slider-ithem'));
+ const products = Array.from(document.querySelectorAll('div.slider-ithem'));
 
 let currIdx = 0;
 
 function showCurrentIthem () {
-   const wareContainer = document.querySelector('.slider');
-   wareContainer.innerHTML = ware[currIdx].innerHTML;
+   const productsContainer = document.querySelector('.slider');
+   productsContainer.innerHTML = products[currIdx].innerHTML;
 }
 showCurrentIthem ();
 function nextIthem () {
 currIdx++
-if (currIdx >= ware.length) currIdx = 0;
+if (currIdx >= products.length) currIdx = 0;
 showCurrentIthem()
 }
 function prevIthem () {
     currIdx--
-    if (currIdx < 0) currIdx= ware.length - 1;
+    if (currIdx < 0) currIdx= products.length - 1;
     showCurrentIthem()
 }
-
-
 document.getElementById('show-prev').addEventListener('click', prevIthem)
 document.getElementById('show-next').addEventListener('click',nextIthem)
